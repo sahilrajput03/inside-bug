fd28c036c216760fc2d36a4b2790f9c35b92ba55
Sahil Rajput <sahilrajput03@gmail.com>
2021-05-23 18:03:50 +0530

    Remove faulty file. ~ sahil

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 const ClienRenderedComponent = dynamic(() => import('../components/TaiTae'), {
 	ssr: false, // I disabled the server rendering this way!!! Thanks to 🥇︎: https://github.com/vercel/next.js/issues/9890#issuecomment-605543395
+	// official docs: https://nextjs.org/docs/advanced-features/dynamic-import#with-no-ssr
 })
 
 const HomePage = () => {
